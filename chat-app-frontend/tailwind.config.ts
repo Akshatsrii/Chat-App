d4805d385ddae3312afd182a612e659173fd40bb
Akshat Srivastava <chikusrivastava535@gmail.com>
2026-04-08 18:13:31 +0530

    update the css

--- chat-app-frontend/tailwind.config.ts
+++ chat-app-frontend/tailwind.config.ts
@@ -1,19 +1,9 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      animation: {
-        "spin-slow": "spin 3s linear infinite",
-        ping: "ping 1s cubic-bezier(0, 0, 0.2, 1) infinite",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
-export default config;
